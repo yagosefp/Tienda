fa08344aba2482f0fdf129d138efedd0c09807c5
yago.suarez <ysuareze@fpcoruna.afundacion.org>
2022-12-03 18:50:26 +0100

    Commit inicial django levantado

---
+++ tienda/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Tipo(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name_plural ='tipos'
+    def __str__(self):
+        return self.name
+    
+class Producto(models.Model):
+    tipo = models.ForeignKey(Tipo, related_name='producto', on_delete=models.CASCADE)
+    creador =models.ForeignKey(User, on_delete=models.CASCADE ,related_name='creador_producto')
+    nombre = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='images/')
+    slug = models.SlugField(max_length=255)
+    precio = models.DecimalField(max_digits=4, decimal_places=2)
+    in_stock = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    creado = models.DateTimeField(auto_now_add=True)
+    modificado = models.DateTimeField(auto_now=True)
+   
+    
+
+    class Meta:
+        verbose_name_plural = 'Productos'
+        ordering = ('-creado',)
+
+    def __str__(self):
+        return self.nombre
+
+  
+
+
+# Create your models here.
