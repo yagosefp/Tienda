dd2047b15cd23832ccac6348a8b8872287d52a2b
yago.suarez <ysuareze@fpcoruna.afundacion.org>
2022-12-03 19:18:03 +0100

    Creado superusuario y articulo de prueba

--- tienda/admin.py
+++ tienda/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tipo,Producto
+
+@admin.register(Tipo)
+class TipoAdmin(admin.ModelAdmin):
+    list_display=['name','slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display=['nombre','creador','slug','precio','in_stock','creado','modificado']
+    list_filter = ['in_stock', 'is_active']
+    list_editable = ['precio', 'in_stock']
+    prepopulated_fields = {'slug': ('nombre',)}
